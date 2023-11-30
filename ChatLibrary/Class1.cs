d25378ce2992dd0ae10022ec76246df70c266415
Viacheslav Fomenko <vt222_fva@student.ztu.edu.ua>
2023-11-30 22:22:34 +0200

    ready

---
+++ ChatLibrary/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace ChatLibrary
+{
+    [ServiceContract(CallbackContract = typeof(IChatServiceCallback))]
+    public interface IChatService
+    {
+        [OperationContract]
+        int Connect(string username);
+        [OperationContract(IsOneWay = true)]
+        void Disconnect(int id);
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+    }
+
+    [ServiceContract]
+    public interface IChatServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendMessageToClient(string message);
+
+        [OperationContract(IsOneWay = true)]
+        void UserConnected(string username);
+
+        [OperationContract(IsOneWay = true)]
+        void UserDisconnected(string username);
+    }
+
+    public class ChatUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public OperationContext Context { get; set; }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ChatService : IChatService
+    {
+        List<ChatUser> usersList = new List<ChatUser>();
+        int nextUserId = 1;
+        public int Connect(string username)
+        {
+            ChatUser newUser = new ChatUser()
+            {
+                Id = nextUserId++,
+                Name = username,
+                Context = OperationContext.Current,
+            };
+            usersList.Add(newUser);
+
+            foreach (ChatUser connectedUser in usersList)
+            {
+                connectedUser.Context.GetCallbackChannel<IChatServiceCallback>().UserConnected(username);
+            }
+
+            return newUser.Id;
+        }
+
+        public void Disconnect(int id)
+        {
+            var disconnectedUser = usersList.FirstOrDefault(x => x.Id == id);
+            if (disconnectedUser != null)
+            {
+                // Уведомите клиента о отключении пользователя
+                foreach (ChatUser otherUser in usersList)
+                {
+                    otherUser.Context.GetCallbackChannel<IChatServiceCallback>().UserDisconnected(disconnectedUser.Name);
+                }
+
+                usersList.Remove(disconnectedUser);
+            }
+        }
+
+
+        public void SendMessage(string message)
+        {
+            foreach (ChatUser user in usersList)
+            {
+                // Измените формат сообщения, чтобы включить имя пользователя
+                user.Context.GetCallbackChannel<IChatServiceCallback>().SendMessageToClient($"{user.Name}: {message}");
+            }
+
+        }
+    }
+}
---
+++ Client/ChatServerConnector.cs
@@ -0,0 +1,63 @@
+﻿using Client.ChatServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public class ChatServerConnector : IChatServiceCallback
+    {      
+        private static ChatServerConnector Instance = null;
+        private ChatServiceClient Client;
+        private int UserId;
+        private string Username;
+
+        private ChatServerConnector()
+        {
+            Client = new ChatServiceClient(new System.ServiceModel.InstanceContext(this));
+        }
+        static ChatServerConnector()
+        {
+            if (Instance == null)
+                Instance = new ChatServerConnector();
+        }
+        public static ChatServerConnector GetInstance()
+        {
+            if (Instance == null)
+                Instance = new ChatServerConnector();
+            return Instance;
+        }
+
+        internal static void Connect(string text)
+        {
+            Instance.Username = text; 
+            Instance.UserId = Instance.Client.Connect(text);
+        }
+        public void SendMessageToServer(string message)
+        {
+            GetInstance().Client.SendMessage($"{Username}: {message}");
+        }
+        public void SendMessageToClient(string message)
+        {
+            (Application.OpenForms["Form1"] as Form1).listBox1.Items.Add(message);
+        }
+
+        public void UserConnected(string username)
+        {
+ 
+            (Application.OpenForms["Form1"] as Form1).listBox2.Items.Add(username);
+        }
+
+        public void UserDisconnected(string username)
+        {
+   
+
+           
+            (Application.OpenForms["Form1"] as Form1).listBox2.Items.Remove(username);
+        }
+
+    }
+}
